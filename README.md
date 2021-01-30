a34f0b1652f01abe33135cab169e6cda5e69944b
Lznah <hanzlpe@icloud.com>
2021-01-30 12:01:01 +0100

    Add license, readme and env. variables

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple Sreality Selenium Scrapper
+
+Set environment variable SREALITY_SCAPPER_DATAPATH, where you want to store data in JSON format and run 'python3 ./run.py'
+
+You need to install chromedriver and provide path into PATH.
--- run.py
+++ run.py
@@ -1,14 +1,19 @@
 import json
 import time
+import os
 from datetime import date
 from classes.page import Page
 from classes.adresar_page import AdresarPage
 
-curr_time = str(int(time.time()))
-print("#Started webscrape on: "+date.today().strftime("%B %d, %Y")+" - file: "+curr_time+".json")
-Page.addPage(AdresarPage("https://www.sreality.cz/adresar?strana=1"))
-Page.run()
-# f = open("/root/statistiky/data/"+curr_time+".json", "w")
-f = open("./"+curr_time+".json", "w")
-f.write(json.dumps(Page.getCompanies()))
-f.close()
+try:
+    data_path = os.environ['SREALITY_SCAPPER_DATAPATH']
+    curr_time = str(int(time.time()))
+    print("#Started webscrape on: "+date.today().strftime("%B %d, %Y")+" - file: "+curr_time+".json")
+    Page.addPage(AdresarPage("https://www.sreality.cz/adresar?strana=1"))
+    Page.run()
+    f = open(data_path+curr_time+".json", "w")
+    f.write(json.dumps(Page.getCompanies()))
+    f.close()
+except KeyError:
+    print('Set SREALITY_SCAPPER_DATAPATH environment variable.')
+
